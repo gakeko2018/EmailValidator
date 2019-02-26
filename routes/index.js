d3ca07c6d47cd62cf97b9541c2cd06fbe8739821
Mustafa S <gakeko2018@gmail.com>
2019-02-27 00:19:48 +0300

    debugged2

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,8 @@
 var express = require("express");
 var router = express.Router();
-const rimraf = require("rimraf");
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  rimraf("./saved/*", function() {
-    console.log("Saved folder cleared.");
-  });
-
   res.render("index", { title: "Express" });
 });
 
